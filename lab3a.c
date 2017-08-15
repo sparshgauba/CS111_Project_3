e4c303c34d1f89a97ac9324966e39448009412ab
Ubuntu User <sean.langley22@gmail.com>
2017-08-15 18:37:49 -0400

    Stuff

--- lab3a.c
+++ lab3a.c
@@ -4,23 +4,32 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <fcntl.h>
+#include <stdint.h>
+
+
+typedef struct superblock{
+  uint32_t num_inodes[256];
+  
+} superblock_t;
+
+
+
 
 int main(int argc, char **argv)
 {
+  superblock_t *myblock;
+  
   char *file = argv[0];
   int fd = open(file, O_RDONLY, 0444);
-  char inodes[1024];
-  pread(fd, inodes, 1024, 1024);
-  int i;
-  for(i = 0; i < 1023; i++)
-    {
-      printf("%X", inodes[i]);
-      if(i % 8 == 0)
-	printf("\n");
-
-    }
-  printf("\n");
+  uint8_t block_read[1024];
+  pread(fd, block_read, 1024, 1024);
+  block_read[1024] = '\0';
   
+  printf("%X\n", block_read);
+  
+  
+
+
   return 0;
        
   
