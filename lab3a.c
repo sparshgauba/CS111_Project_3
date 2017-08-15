795a6ccbb7b2d398ff5270cfa89191c39ff58f18
Ubuntu User <sean.langley22@gmail.com>
2017-08-15 19:37:24 -0400

    MOre stuff

--- lab3a.c
+++ lab3a.c
@@ -5,27 +5,42 @@
 #include <sys/types.h>
 #include <fcntl.h>
 #include <stdint.h>
-
-
+#include <errno.h>
+#include <stdlib.h>
 typedef struct superblock{
-  uint32_t num_inodes[256];
-  
+  uint32_t num_inodes;
+  char temp[1020];
 } superblock_t;
 
 
-
+void exit_1(char *str)
+{
+  fprintf(stderr, "Error: ");
+  if(strlen(str) != 0)
+    fprintf(stderr, "%s\n", str);
+  if(errno != 0)
+    perror("");
+  fprintf(stderr, "Exiting with error code 1.\n");
+  exit(1);
+  
+   
+}
 
 int main(int argc, char **argv)
 {
-  superblock_t *myblock;
-  
-  char *file = argv[0];
+  superblock_t *myblock;  
+  char *file = argv[1];
   int fd = open(file, O_RDONLY, 0444);
+
   uint8_t block_read[1024];
-  pread(fd, block_read, 1024, 1024);
-  block_read[1024] = '\0';
+  if(!pread(fd, block_read, 1024, 1024))
+    exit_1("");
+  myblock = (superblock_t*) block_read;
+  int i;
+  printf("num inodes %d\n", myblock->num_inodes);
   
-  printf("%X\n", block_read);
+  
+  printf("\n");
   
   
 
