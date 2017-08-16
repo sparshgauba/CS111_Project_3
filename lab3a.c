16366c6c341fd2041cc7bbb4dd3625eb88432ff1
Ubuntu User <sean.langley22@gmail.com>
2017-08-16 17:21:08 -0400

    Modularize the bitmap portions

--- lab3a.c
+++ lab3a.c
@@ -70,14 +70,14 @@ int is_bit_set(char byte, int index)
 {
   switch(index)
     {
-    case 0: return (byte & 128) >> 7;
-    case 1: return (byte & 64) >> 6;
-    case 2: return (byte & 32) >> 5;
-    case 3: return (byte & 16) >> 4;
-    case 4: return (byte & 8) >> 3;
-    case 5: return (byte & 4) >> 2;
-    case 6: return (byte & 2) >> 1;
-    case 7: return (byte & 1);
+    case 0: return (byte & 0x80) >> 7;
+    case 1: return (byte & 0x40) >> 6;
+    case 2: return (byte & 0x20) >> 5;
+    case 3: return (byte & 0x10) >> 4;
+    case 4: return (byte & 0x8) >> 3;
+    case 5: return (byte & 0x4) >> 2;
+    case 6: return (byte & 0x2) >> 1;
+    case 7: return (byte & 0x1);
     }
   return -1;
 }
@@ -91,7 +91,32 @@ void superblock_info(int fd)
   superblock_output();
 }
 
+void free_bits(char map_read[], int num_iterations, int block_flag)
+{
+  int i;
+  for(i = 0; i < num_iterations; i++)
+    {
+          /*Cycle through each of the bits in a given byte*/
+    int j;
+    for(j = 0; j < 8; j++)
+      {
+  	int bit = is_bit_set(map_read[i],j);
+  	if(! bit)
+  	  {
+  	    int index_free = i * 8 + j;
+	    if(block_flag)
+	      printf("BFREE,%d\n", index_free);
+	    else
+	      printf("IFREE,%d\n", index_free);
+	    
+  	  }
+  	else if(bit == -1)
+  	  exit_1("Error with bit-map.");
+	       
+      }
 
+    }
+}
 
 int main(int argc, char **argv)
 {
@@ -119,61 +144,35 @@ int main(int argc, char **argv)
   
   //TODO: IMPLEMENT FOR MORE THAN 1 BLOCK GROUP
   /*Get the block number where the bitmap is*/
-  int i;
+
   char blockmap[BLOCKSIZE];
   int blockmap_byte = groupdescriptor_ptr->bg_block_bitmap * BLOCKSIZE;
   if(pread(fd, blockmap, BLOCKSIZE, blockmap_byte) == -1)
     exit_1("");
   
-    /*Account for variable group size*/
+  /*Account for variable group size*/
   //each byte is 8 bits
   int num_iterations = superblock_ptr->s_blocks_per_group / 8;
-  for(i = 0; i < num_iterations; i++)
-  {
-    /*Cycle through each of the bits in a given byte*/
-    int j;
-    for(j = 0; j < 8; j++)
-      {
-  	int bit = is_bit_set(blockmap[i],j);
-  	if(! bit)
-  	  {
-  	    int block = i * 8 + j;
-  	    printf("BFREE,%d\n", block);
-	    
-  	  }
-  	else if(bit == -1)
-  	  exit_1("Error with block bit-map.");
-	       
-      }
-  }
+  int block_flag = 1;
+  free_bits(blockmap, num_iterations, block_flag);
 
   
-
+  /*Get the free inodes, pretty much the same implementation as free blocks*/
   char inodemap[BLOCKSIZE];
   int inodemap_byte = groupdescriptor_ptr->bg_inode_bitmap * BLOCKSIZE;
   if(pread(fd, inodemap, BLOCKSIZE, inodemap_byte) == -1)
     exit_1("");
-
   num_iterations = superblock_ptr->s_inodes_per_group / 8;
-  for(i = 0; i < num_iterations; i++)
-    {
-      int j;
-      for(j = 0; j < 8; j++)
-	{
-	  int bit = is_bit_set(inodemap[i],j);
-	  if(! bit)
-	    {
-	      int inode = i * 8 + j;
-	      printf("IFREE,%d\n", inode);
-	    }
-	  else if(bit == -1)
-	    exit_1("Error with inode bitmap.");
-	}
-    }
+  block_flag = 0;
+  free_bits(inodemap, num_iterations, block_flag);
+
+  /*START INODE SUMMARY HERE*/
   
+
   
 
   exit(0);
+  
        
   
 }
